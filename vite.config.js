66bb57501def89b526a7dd0206d1bf9819bdb951
blackwhaledev <alizade.t488@gmail.com>
2022-10-22 04:32:03 +0300

    first vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+const path = require('path')
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -13,5 +15,16 @@ export default defineConfig({
     ],
     ssr: {
         noExternal: ['@inertiajs/server'],
+    },
+    resolve:{
+        alias:{
+            "@Components": path.resolve(`resources/js/Components`),
+            "@Contexts": path.resolve(`resources/js/Contexts`),
+            "@Layouts": path.resolve(`resources/js/Layouts`),
+            "@Pages": path.resolve(`resources/js/Pages`),
+            "@Styles": path.resolve(`resources/js/Styles`),
+            "@Utils": path.resolve(`resources/js/Utils`),
+            "@Hooks": path.resolve(`resources/js/Hooks`),
+        },
     },
 });
